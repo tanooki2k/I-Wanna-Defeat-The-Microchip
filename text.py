971dbd585009de179a180a8f70545a22b7d4b084
Javier Rojas Ramírez <javierito.rojas.ramirez@gmail.com>
2024-04-05 23:18:45 +0100

    Class for Text ended

--- text.py
+++ text.py
@@ -2,14 +2,42 @@ import pygame
 
 
 class Text:
-    def __init__(self, text, font, position, size, color, gap=False):
-        self.font = pygame.font.SysFont(font, size)
-
-        self.title = self.font.render(text, True, color)
-        self.gap = gap
-
+    def __init__(self, position, animate):
         if not isinstance(position, list):
-            raise ValueError(f"You tried to use {position.__class__.__name__} when list is required")
+            raise ValueError(f'You tried to use "{position.__class__.__name__}" when list is required')
         elif len(position) != 2:
             raise IndexError(f"You need just 2 values for (x, y), but you put {len(position)}")
         self.position = position
+
+        self.animation = animate
+        self.counter = 0
+        self.timer = 4
+
+    def draw(self, screen):
+        if self.animation:
+            self.counter += 1
+            if self.counter == self.timer * 2:
+                self.counter = 0
+            if self.counter >= self.timer:
+                return
+        screen.blit(self.content, self.position)
+
+
+class SysFont(Text):
+    def __init__(self, text, font, position, size, color, animate=0):
+        super().__init__(position, animate)
+        self.font = pygame.font.SysFont(font, size)
+
+        if len(color) != 3:
+            raise IndexError(f"You need just 3 values for (red, green, blue), but you put {len(position)}")
+        self.content = self.font.render(text, True, color)
+
+
+class Font(Text):
+    def __init__(self, text, font, position, size, color, animate=0):
+        super().__init__(position, animate)
+        self.font = pygame.font.Font(f'fonts/{font}', size)
+
+        if len(color) != 3:
+            raise IndexError(f"You need just 3 values for (red, green, blue), but you put {len(position)}")
+        self.content = self.font.render(text, True, color)
--- title.py
+++ title.py
@@ -1,8 +1,15 @@
 import pygame
+from text import SysFont
+from text import Font
 
 
 class Title:
     def __init__(self, screen):
+        self.title_1 = SysFont("I Wanna Defeat", 'Georgia', [0, 0], 80, (255, 255, 255))
+        self.title_2 = SysFont("The Microchip", 'Georgia', [0, 100], 80, (255, 255, 255))
+        self.press_enter = Font('Press "Enter"', 'CampanaScript.otf', [0, 200], 40, (255, 255, 255), 1)
+
+        self.texts = [self.title_1, self.title_2, self.press_enter]
         self.game_play_scene = None
 
     def update(self, events):
@@ -13,4 +20,5 @@ class Title:
         return self
 
     def draw(self, screen):
-        screen.blit(self.title, self.position)
+        for text in self.texts:
+            text.draw(screen)
