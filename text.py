879ba6bf728a91427633c03ab35adf1f087cf908
Javier Rojas Ramírez <javierito.rojas.ramirez@gmail.com>
2024-04-06 21:45:28 +0100

    First Version of Title!

--- text.py
+++ text.py
@@ -2,42 +2,75 @@ import pygame
 
 
 class Text:
-    def __init__(self, position, animate):
+    """
+    Defining a "Text"
+    """
+
+    def __init__(self, font, text, color, position, animate):
+        self.font = font
+
+        if len(color) != 3:
+            raise IndexError(f"You need just 3 values for (red, green, blue), but you put {len(position)}")
+        self.content = self.font.render(text, True, color)
+
         if not isinstance(position, list):
             raise ValueError(f'You tried to use "{position.__class__.__name__}" when list is required')
         elif len(position) != 2:
             raise IndexError(f"You need just 2 values for (x, y), but you put {len(position)}")
         self.position = position
+        self.index = {
+            "x": 0,
+            "y": 1
+        }
 
-        self.animation = animate
+        self.define_animation = {
+            "0": True,
+            "1": Text.animate_1
+        }
+        self.animate = self.define_animation[str(animate)]
+        self.blit = True
         self.counter = 0
-        self.timer = 4
+        self.timer = 5
+
+    def __getitem__(self, item):
+        return self.position[self.index[item]]
+
+    def width(self):
+        return self.content.get_width()
+
+    def height(self):
+        return self.content.get_height()
 
     def draw(self, screen):
-        if self.animation:
-            self.counter += 1
-            if self.counter == self.timer * 2:
-                self.counter = 0
-            if self.counter >= self.timer:
-                return
-        screen.blit(self.content, self.position)
+        self.blit = True
+        if not isinstance(self.animate, bool):
+            self.blit = self.animate(self)
+        if self.blit and self.animate:
+            screen.blit(self.content, self.position)
+
+    # Methods for animate text
+    def animate_1(self):
+        self.counter += 1
+        if self.counter == self.timer * 2:
+            self.counter = 0
+        if self.counter >= self.timer:
+            return False
+        return True
 
 
 class SysFont(Text):
-    def __init__(self, text, font, position, size, color, animate=0):
-        super().__init__(position, animate)
-        self.font = pygame.font.SysFont(font, size)
+    """
+    For calling Fonts in Python's defaults fonts
+    """
 
-        if len(color) != 3:
-            raise IndexError(f"You need just 3 values for (red, green, blue), but you put {len(position)}")
-        self.content = self.font.render(text, True, color)
+    def __init__(self, text, font, size, color, position, animate=0):
+        super().__init__(pygame.font.SysFont(font, size), text, color, position, animate)
 
 
 class Font(Text):
-    def __init__(self, text, font, position, size, color, animate=0):
-        super().__init__(position, animate)
-        self.font = pygame.font.Font(f'fonts/{font}', size)
+    """
+    For calling external Fonts
+    """
 
-        if len(color) != 3:
-            raise IndexError(f"You need just 3 values for (red, green, blue), but you put {len(position)}")
-        self.content = self.font.render(text, True, color)
+    def __init__(self, text, font, size, color, position, animate=0):
+        super().__init__(pygame.font.Font(f'fonts/{font}', size), text, color, position, animate)
--- title.py
+++ title.py
@@ -5,18 +5,34 @@ from text import Font
 
 class Title:
     def __init__(self, screen):
-        self.title_1 = SysFont("I Wanna Defeat", 'Georgia', [0, 0], 80, (255, 255, 255))
-        self.title_2 = SysFont("The Microchip", 'Georgia', [0, 100], 80, (255, 255, 255))
-        self.press_enter = Font('Press "Enter"', 'CampanaScript.otf', [0, 200], 40, (255, 255, 255), 1)
+        self.title_1 = SysFont("I Wanna Defeat", 'Georgia', 80, (255, 255, 255), [0, 0])
+        self.title_2 = SysFont("The Microchip", 'Georgia', 75, (255, 255, 255), [0, 0])
+        self.press_enter = Font('Press "Enter"', 'CampanaScript.otf', 70, (255, 255, 255), [0, 0], 1)
+
+        self.gap_1 = 80 - self.title_1.height()
+        self.gap_2 = 170 - 80 - self.title_2.height()
+
+        self.title_1.position = [
+            (screen.get_width() - self.title_1.width()) / 2,
+            (screen.get_height() - self.title_1.height() - self.title_2.height() - self.press_enter.height() - self.gap_1 - self.gap_2) / 2
+        ]
+        self.title_2.position = [
+            (screen.get_width() - self.title_2.width()) / 2,
+            self.title_1["y"] + self.title_1.height() + self.gap_1
+        ]
+        self.press_enter.position = [
+            (screen.get_width() - self.press_enter.width()) / 2,
+            self.title_2["y"] + self.title_2.height() + self.gap_2
+        ]
 
         self.texts = [self.title_1, self.title_2, self.press_enter]
-        self.game_play_scene = None
+        self.menu = None
 
     def update(self, events):
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
-                    return self.game_play_scene
+                    return self.menu
         return self
 
     def draw(self, screen):
