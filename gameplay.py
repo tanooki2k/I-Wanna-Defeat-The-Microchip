cc0533a538133e96f48d24ec60609e0add20c670
Javier Rojas Ramírez <javierito.rojas.ramirez@gmail.com>
2024-05-26 12:22:54 +0100

    Wee changes

--- gameplay.py
+++ gameplay.py
@@ -8,10 +8,10 @@ class Gameplay:
     def __init__(self, screen):
         # Creating the Background:
         self.background = Background("background.png", [0, 0], settings.screen_width / 537, settings.screen_height / 442)
-        self.capsule = Background("capsule.png", [(settings.screen_width - 465) / 2, 0])
+        self.capsule = Background("capsule.png", [(settings.screen_width - 465) / 2, 0], (settings.screen_height - 100) / 600, (settings.screen_height - 100) / 600)
 
         # Initializing  the Player:
-        self.player = Player(200, 558)
+        self.player = Player(200, settings.screen_height - 142)
 
         self.pause = Font('Pause', 'CampanaScript.otf', int(screen.get_height() * (100 / 700)), (255, 255, 255), [0, 0])
         self.pause.position = [
@@ -36,8 +36,8 @@ class Gameplay:
     def draw(self, screen):
         self.background.draw(screen)
         self.capsule.draw(screen)
-        pygame.draw.rect(screen, (0, 0, 0), [0, 600, screen.get_width(), 100])
-        pygame.draw.rect(screen, settings.floor_color, [0, 605, screen.get_width(), 100])
+        pygame.draw.rect(screen, (0, 0, 0), [0, settings.screen_height-100, screen.get_width(), 100])
+        pygame.draw.rect(screen, settings.floor_color, [0, settings.screen_height-95, screen.get_width(), 100])
         self.player.draw(screen)
         if self.is_pause % 2:
             for text in self.texts:
